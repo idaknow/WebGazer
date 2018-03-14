902b60f48ee65251e432fa5d1a2173b2145cc3a9
Ida <ides542@aucklanduni.ac.nz>
2018-03-14 18:39:27 -0400

    changed sweetalert to version sweetalert2

--- www/js/calibration.js
+++ www/js/calibration.js
@@ -18,12 +18,8 @@ function PopUpInstruction(){
   swal({
     title:"Calibration",
     text: "Please click on each of the 9 points on the screen. You must click on each point 5 times till it goes yellow. This will calibrate your eye movements.",
-    buttons:{
-      cancel: false,
-      confirm: true
-    }
   }).then(isConfirm => {
-    ShowCalibrationPoint();
+    isConfirm.value && ShowCalibrationPoint();
   });
 
 }
@@ -79,7 +75,7 @@ $(document).ready(function(){
             swal({
               title: "Calculating measurement",
               text: "Please don't move your mouse & stare at the middle dot for the next 5 seconds. This will allow us to calculate the accuracy of our predictions.",
-              closeOnEsc: false,
+              allowEscapeKey: false,
               allowOutsideClick: false,
               closeModal: true
             }).then( isConfirm => {
@@ -98,12 +94,10 @@ $(document).ready(function(){
                       swal({
                         title: "Your accuracy measure is " + precision_measurement + "%",
                         allowOutsideClick: false,
-                        buttons: {
-                          cancel: "Recalibrate",
-                          confirm: true,
-                        }
+                        showCancelButton: true,
+                        cancelButtonText: "Recalibrate"
                       }).then(isConfirm => {
-                          if (isConfirm){
+                          if (isConfirm.value){
                             //clear the calibration & hide the last middle button
                             ClearCanvas();
                           } else {
